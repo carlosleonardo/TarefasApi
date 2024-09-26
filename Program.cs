ec2daf912ed032a5a55a27c950f622e684fb69b6
Carlos Leonardo <carlos.leonardo.goes@gmail.com>
2024-09-26 09:43:57 -0300

    Alterado para usar funções estáticas no lugar de lambdas

--- Program.cs
+++ Program.cs
@@ -29,50 +29,62 @@ if (app.Environment.IsDevelopment())
 
 var tarefasItens = app.MapGroup("/tarefas");
 
-tarefasItens.MapGet("/", async (TarefaDb db) =>
-{
-	return await db.Tarefas.ToListAsync();
-});
+tarefasItens.MapGet("/", ObterTarefas);
 
-tarefasItens.MapGet("/complete", async (TarefaDb db) =>
+static async Task<IResult> ObterTarefas(TarefaDb db)
 {
-	return await db.Tarefas.Where(t => t.IsComplete).ToListAsync();
-});
+	return TypedResults.Ok(await db.Tarefas.ToListAsync());
+}
 
-tarefasItens.MapGet("/{id}", async (int id, TarefaDb db) =>
+tarefasItens.MapGet("/complete", ObterTarefasCompletas);
+
+static async Task<IResult> ObterTarefasCompletas(TarefaDb db)
+{
+	return TypedResults.Ok(await db.Tarefas.Where(t => t.IsComplete).ToListAsync());
+}
+
+tarefasItens.MapGet("/{id}", ObterTarefaPorid);
+
+static async Task<IResult> ObterTarefaPorid(int id, TarefaDb db)
 {
 	return await db.Tarefas.FindAsync(id)
-		is Tarefa tarefa
-		? Results.Ok(tarefa)
-		: Results.NotFound();
-});
+			is Tarefa tarefa
+			? TypedResults.Ok(tarefa)
+			: TypedResults.NotFound();
+}
 
-tarefasItens.MapPost("/", async (Tarefa tarefa, TarefaDb db) =>
+tarefasItens.MapPost("/", CriarTarefa);
+
+static async Task<IResult> CriarTarefa(Tarefa tarefa, TarefaDb db)
 {
 	db.Tarefas.Add(tarefa);
 	await db.SaveChangesAsync();
-	return Results.Created($"/tarefas/{tarefa.Id}", tarefa);
-});
+	return TypedResults.Created($"/tarefas/{tarefa.Id}", tarefa);
+}
 
-tarefasItens.MapPut("/{id}", async (int id, Tarefa inputTarefa, TarefaDb db) =>
+tarefasItens.MapPut("/{id}", AtualizarTarefa);
+
+static async Task<IResult> AtualizarTarefa(int id, Tarefa inputTarefa, TarefaDb db)
 {
 	var tarefa = await db.Tarefas.FindAsync(id);
-	if (tarefa is null) return Results.NotFound();
+	if (tarefa is null) return TypedResults.NotFound();
 	tarefa.Name = inputTarefa.Name;
 	tarefa.IsComplete = inputTarefa.IsComplete;
 	await db.SaveChangesAsync();
-	return Results.NoContent();
-});
+	return TypedResults.NoContent();
+}
 
-tarefasItens.MapDelete("/{id}", async (int id, TarefaDb db) =>
+tarefasItens.MapDelete("/{id}", ExcluirTarefa);
+
+static async Task<IResult> ExcluirTarefa(int id, TarefaDb db)
 {
 	if (await db.Tarefas.FindAsync(id) is Tarefa tarefa)
 	{
 		db.Tarefas.Remove(tarefa);
 		await db.SaveChangesAsync();
-		return Results.NoContent();
+		return TypedResults.NoContent();
 	}
-	return Results.NotFound();
-});
+	return TypedResults.NotFound();
+}
 
 app.Run();
